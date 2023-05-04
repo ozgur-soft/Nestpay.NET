cffffb98e1ce9ea4f3131e19c423e3ccf70d298c
Özgür Yalçın <ozguryalcin@outlook.com>
2023-05-04 21:05:16 +0300

    Update Nestpay.cs

--- Nestpay/Nestpay.cs
+++ Nestpay/Nestpay.cs
@@ -15,25 +15,7 @@ namespace Nestpay {
         Finansbank,
         Teb
     }
-    public interface INestpay {
-        void SetMode(string mode);
-        void SetClientID(string clientid);
-        void SetUsername(string username);
-        void SetPassword(string password);
-        void SetIPv4(string ipv4);
-        void SetOrderID(string orderid);
-        void SetAmount(string amount, string currency);
-        void SetInstallment(string installment);
-        void SetCardHolder(string cardholder);
-        void SetPhoneNumber(string phonenumber);
-        void SetCardNumber(string cardnumber);
-        void SetCardExpiry(string cardmonth, string cardyear);
-        void SetCardCode(string cardcode);
-        Nestpay.CC5Response Pay();
-        Nestpay.CC5Response Refund();
-        Nestpay.CC5Response Cancel();
-    }
-    public class Nestpay : INestpay {
+    public class Nestpay {
         private string Endpoint { get; set; }
         private string Mode { get; set; }
         private string ClientID { get; set; }
@@ -66,73 +48,121 @@ namespace Nestpay {
         [Serializable, XmlRoot("CC5Request")]
         public class CC5Request {
             [XmlElement("Mode", IsNullable = false)]
-            public string Mode { init; get; }
+            public string Mode { set; get; }
             [XmlElement("Type", IsNullable = false)]
-            public string Type { init; get; }
+            public string Type { set; get; }
             [XmlElement("Name", IsNullable = false)]
-            public string Username { init; get; }
+            public string Username { set; get; }
             [XmlElement("Password", IsNullable = false)]
-            public string Password { init; get; }
+            public string Password { set; get; }
             [XmlElement("ClientId", IsNullable = false)]
-            public string ClientID { init; get; }
+            public string ClientID { set; get; }
             [XmlElement("OrderId", IsNullable = false)]
-            public string OrderID { init; get; }
+            public string OrderID { set; get; }
             [XmlElement("GroupId", IsNullable = false)]
-            public string GroupID { init; get; }
+            public string GroupID { set; get; }
             [XmlElement("TransId", IsNullable = false)]
-            public string TransID { init; get; }
+            public string TransID { set; get; }
             [XmlElement("UserId", IsNullable = false)]
-            public string UserID { init; get; }
+            public string UserID { set; get; }
             [XmlElement("IPAddress", IsNullable = false)]
-            public string IPAddress { init; get; }
+            public string IPAddress { set; get; }
             [XmlElement("Email", IsNullable = false)]
-            public string Email { init; get; }
+            public string Email { set; get; }
             [XmlElement("Number", IsNullable = false)]
-            public string CardNumber { init; get; }
+            public string CardNumber { set; get; }
             [XmlElement("Expires", IsNullable = false)]
-            public string CardExpiry { init; get; }
+            public string CardExpiry { set; get; }
             [XmlElement("Cvv2Val", IsNullable = false)]
-            public string CardCode { init; get; }
+            public string CardCode { set; get; }
             [XmlElement("Total", IsNullable = false)]
-            public string Amount { init; get; }
+            public string Amount { set; get; }
             [XmlElement("Currency", IsNullable = false)]
-            public string Currency { init; get; }
+            public string Currency { set; get; }
             [XmlElement("Instalment", IsNullable = false)]
-            public string Installment { init; get; }
+            public string Installment { set; get; }
             [XmlElement("PayerTxnId", IsNullable = false)]
-            public string PayerTxnId { init; get; }
+            public string PayerTxnId { set; get; }
             [XmlElement("PayerSecurityLevel", IsNullable = false)]
-            public string PayerSecurityLevel { init; get; }
+            public string PayerSecurityLevel { set; get; }
             [XmlElement("PayerAuthenticationCode", IsNullable = false)]
-            public string PayerAuthenticationCode { init; get; }
+            public string PayerAuthenticationCode { set; get; }
             [XmlElement("CardholderPresentCode", IsNullable = false)]
-            public string CardholderPresentCode { init; get; }
+            public string CardholderPresentCode { set; get; }
             [XmlElement("BillTo", IsNullable = false)]
-            public To BillTo { init; get; }
+            public To BillTo { set; get; }
             [XmlElement("ShipTo", IsNullable = false)]
-            public To ShipTo { init; get; }
+            public To ShipTo { set; get; }
+            internal void SetMode(string mode) {
+                Mode = mode switch {
+                    "TEST" => "T",
+                    "PROD" => "P",
+                    _ => mode
+                };
+            }
+            internal void SetClientId(string clientid) {
+                ClientID = clientid;
+            }
+            internal void SetUsername(string username) {
+                Username = username;
+            }
+            internal void SetPassword(string password) {
+                Password = password;
+            }
+            internal void SetIPv4(string ipv4) {
+                IPAddress = ipv4;
+            }
+            internal void SetOrderId(string orderid) {
+                OrderID = orderid;
+            }
+            internal void SetAmount(string amount, string currency) {
+                Amount = amount;
+                Currency = currency switch {
+                    "TRY" => "949",
+                    "YTL" => "949",
+                    "TRL" => "949",
+                    "TL" => "949",
+                    "USD" => "840",
+                    "EUR" => "978",
+                    "GBP" => "826",
+                    "JPY" => "392",
+                    _ => currency
+                };
+            }
+            internal void SetInstallment(string installment) {
+                Installment = installment;
+            }
+            internal void SetCardNumber(string cardnumber) {
+                CardNumber = cardnumber;
+            }
+            internal void SetCardExpiry(string cardmonth, string cardyear) {
+                CardExpiry = cardmonth + "/" + cardyear;
+            }
+            internal void SetCardCode(string cardcode) {
+                CardCode = cardcode;
+            }
         }
         public class To {
             [XmlElement("Name", IsNullable = false)]
-            public string Name { init; get; }
+            public string Name { set; get; }
             [XmlElement("Company", IsNullable = false)]
-            public string Company { init; get; }
+            public string Company { set; get; }
             [XmlElement("Street1", IsNullable = false)]
-            public string Street1 { init; get; }
+            public string Street1 { set; get; }
             [XmlElement("Street2", IsNullable = false)]
-            public string Street2 { init; get; }
+            public string Street2 { set; get; }
             [XmlElement("Street3", IsNullable = false)]
-            public string Street3 { init; get; }
+            public string Street3 { set; get; }
             [XmlElement("City", IsNullable = false)]
-            public string City { init; get; }
+            public string City { set; get; }
             [XmlElement("StateProv", IsNullable = false)]
-            public string StateProv { init; get; }
+            public string StateProv { set; get; }
             [XmlElement("PostalCode", IsNullable = false)]
-            public string PostalCode { init; get; }
+            public string PostalCode { set; get; }
             [XmlElement("Country", IsNullable = false)]
-            public string Country { init; get; }
+            public string Country { set; get; }
             [XmlElement("TelVoice", IsNullable = false)]
-            public string TelVoice { init; get; }
+            public string TelVoice { set; get; }
         }
         [Serializable, XmlRoot("CC5Response")]
         public class CC5Response {
@@ -156,141 +186,19 @@ namespace Nestpay {
         public class Writer : StringWriter {
             public override Encoding Encoding => Encoding.UTF8;
         }
-        public void SetMode(string mode) {
-            Mode = mode switch {
-                "TEST" => "T",
-                "PROD" => "P",
-                _ => mode
-            };
+        public CC5Response Auth(CC5Request data) {
+            data.Type = "Auth";
+            return Transaction(data);
         }
-        public void SetClientID(string clientid) {
-            ClientID = clientid;
+        public CC5Response Refund(CC5Request data) {
+            data.Type = "Credit";
+            return Transaction(data);
         }
-        public void SetUsername(string username) {
-            Username = username;
+        public CC5Response Cancel(CC5Request data) {
+            data.Type = "Void";
+            return Transaction(data);
         }
-        public void SetPassword(string password) {
-            Password = password;
-        }
-        public void SetIPv4(string ipv4) {
-            IPv4 = ipv4;
-        }
-        public void SetOrderID(string orderid) {
-            OrderID = orderid;
-        }
-        public void SetAmount(string amount, string currency) {
-            Amount = amount;
-            Currency = currency switch {
-                "TRY" => "949",
-                "YTL" => "949",
-                "TRL" => "949",
-                "TL" => "949",
-                "USD" => "840",
-                "EUR" => "978",
-                "GBP" => "826",
-                "JPY" => "392",
-                _ => currency
-            };
-        }
-        public void SetInstallment(string installment) {
-            Installment = installment;
-        }
-        public void SetCardHolder(string cardholder) {
-            CardHolder = cardholder;
-        }
-        public void SetPhoneNumber(string phonenumber) {
-            PhoneNumber = phonenumber;
-        }
-        public void SetCardNumber(string cardnumber) {
-            CardNumber = cardnumber;
-        }
-        public void SetCardExpiry(string cardmonth, string cardyear) {
-            CardMonth = cardmonth;
-            CardYear = cardyear;
-        }
-        public void SetCardCode(string cardcode) {
-            CardCode = cardcode;
-        }
-        public CC5Response Pay() {
-            var data = new CC5Request {
-                Mode = Mode ?? "P",
-                Type = "Auth",
-                ClientID = ClientID,
-                Username = Username,
-                Password = Password,
-                IPAddress = IPv4,
-                BillTo = new To { Name = CardHolder, TelVoice = PhoneNumber ?? "" },
-                CardNumber = CardNumber,
-                CardExpiry = CardMonth + "/" + CardYear,
-                CardCode = CardCode,
-                Amount = Amount,
-                Currency = Currency,
-                Installment = Installment,
-            };
-            var cc5request = new XmlSerializer(typeof(CC5Request));
-            var cc5response = new XmlSerializer(typeof(CC5Response));
-            using var writer = new Writer();
-            var ns = new XmlSerializerNamespaces();
-            ns.Add(string.Empty, string.Empty);
-            cc5request.Serialize(writer, data, ns);
-            try {
-                using var http = new HttpClient();
-                using var request = new HttpRequestMessage(HttpMethod.Post, Endpoint + "/fim/api") { Content = new StringContent(writer.ToString(), Encoding.UTF8, "text/xml") };
-                using var response = http.Send(request);
-                var result = (CC5Response)cc5response.Deserialize(response.Content.ReadAsStream());
-                return result;
-            } catch (Exception err) {
-                if (err.InnerException != null) {
-                    Console.WriteLine(err.InnerException.Message);
-                } else {
-                    Console.WriteLine(err.Message);
-                }
-            }
-            return null;
-        }
-        public CC5Response Refund() {
-            var data = new CC5Request {
-                Mode = Mode ?? "P",
-                Type = "Credit",
-                ClientID = ClientID,
-                Username = Username,
-                Password = Password,
-                OrderID = OrderID,
-                Amount = Amount,
-                Currency = Currency,
-            };
-            var cc5request = new XmlSerializer(typeof(CC5Request));
-            var cc5response = new XmlSerializer(typeof(CC5Response));
-            using var writer = new Writer();
-            var ns = new XmlSerializerNamespaces();
-            ns.Add(string.Empty, string.Empty);
-            cc5request.Serialize(writer, data, ns);
-            try {
-                using var http = new HttpClient();
-                using var request = new HttpRequestMessage(HttpMethod.Post, Endpoint + "/fim/api") { Content = new StringContent(writer.ToString(), Encoding.UTF8, "text/xml") };
-                using var response = http.Send(request);
-                var result = (CC5Response)cc5response.Deserialize(response.Content.ReadAsStream());
-                return result;
-            } catch (Exception err) {
-                if (err.InnerException != null) {
-                    Console.WriteLine(err.InnerException.Message);
-                } else {
-                    Console.WriteLine(err.Message);
-                }
-            }
-            return null;
-        }
-        public CC5Response Cancel() {
-            var data = new CC5Request {
-                Mode = Mode ?? "P",
-                Type = "Void",
-                ClientID = ClientID,
-                Username = Username,
-                Password = Password,
-                OrderID = OrderID,
-                Amount = Amount,
-                Currency = Currency,
-            };
+        public CC5Response Transaction(CC5Request data) {
             var cc5request = new XmlSerializer(typeof(CC5Request));
             var cc5response = new XmlSerializer(typeof(CC5Response));
             using var writer = new Writer();
